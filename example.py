a28812ead1100612faf708ae1c90f8a89a9df498
ashishcodes619 <ashish.parihar619@gmail.com>
2024-12-29 21:26:19 +0530

    third commit this is

---
+++ example.py
@@ -0,0 +1,4 @@
+# git is very important
+# git add - this command is used to add in git
+# git commit -m "message here" - used to commit a code
+# git push origin main - used to push the code to github.com
