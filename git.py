94e5fc1d470a65e48c959c092989256e48250c42
ashishcodes619 <ashish.parihar619@gmail.com>
2024-12-31 21:18:46 +0530

    understanding branch mechanism

---
+++ git.py
@@ -0,0 +1,3 @@
+# GIt branch - this command is used to list git branches
+# Git branch branch_name - this command is used tp create branch
+# git checkout branch_name - this command is used to switch from one branch to another
